64d6619b98daa18d32d2f88030e71377ff8d8d81
notshahzad <shahzadakhtar182005@gmail.com>
2021-05-24 13:37:26 +0530

    smol-chamge

--- listener.js
+++ listener.js
@@ -11,8 +11,8 @@ server.on("message", (msg, rinfo) => {
     var tilt_side = vals[1];
     var tilt_front = vals[2];
     if (tilt_side > -4 && tilt_side < 4 && tilt_front > -4 && tilt_front < 4) {
-      posx = 200;
-      posy = 200;
+      posx = 0;
+      posy = 0;
     }
     if (tilt_side <= -4) {
       posx = 5;
@@ -27,5 +27,7 @@ server.on("message", (msg, rinfo) => {
     var python = spawn("py", ["mouse.py", `changepos(${posx},${posy})`]);
   }
 });
-server.on("listening", () => {});
+server.on("listening", () => {
+    console.log("listening");
+});
 server.bind(4000);
