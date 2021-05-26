b929e0bd5a10eabc127fa46d673aa1b3118a4322
notshahzad <shahzadakhtar182005@gmail.com>
2021-05-27 01:19:19 +0530

    za_sawa

---
+++ server.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const {
+  gamelogin,
+  UsersInWaiting,
+  Offliner,
+  GetUserById,
+  CheckAuth,
+  UsersInCurrentRoom,
+  sendchitti,
+  GenerateChitti,
+  UserToStart,
+  ChangeUserId,
+  ChitthiPass,
+} = require("./utils/gamelogic");
+
+const app = express();
+const HttpServer = http.createServer(app);
+const io = socketio(HttpServer);
+const bodyParser = require("body-parser");
+const { mainModule } = require("process");
+
+var destination = "/index.html";
+
+app.use(express.static(`${__dirname}/views`));
+app.use(bodyParser.urlencoded({ extended: true }));
+//app.set('view engine','ejs')
+
+io.on("connect", (socket) => {
+  const id = socket.id;
+  //console.log(id)
+  socket.on("login", ({ name, room }) => {
+    login = gamelogin({ name, room, id });
+    if (login != false) {
+      socket.join(room);
+      waiting = UsersInWaiting(room);
+      io.to(room).emit("send_users", waiting);
+      var allusers = UsersInCurrentRoom(room);
+      var is_start;
+      if (allusers.length == 4) {
+        is_start = true;
+        io.to(room).emit("game_start", is_start);
+      } else {
+        is_start = false;
+        io.to(room).emit("game_start", is_start);
+      }
+    } else {
+      socket.emit("redirect", destination);
+    }
+  });
+  socket.on("Start-game", ({ name, room }) => {
+    user = CheckAuth({ name, room });
+    if (user != false) {
+      ChangeUserId(name, socket.id);
+      socket.join(user.room);
+      socket.broadcast.to(user.room).emit("auto-submit");
+      GenerateChitti(room);
+      const chitthi2send = sendchitti(name, room);
+      socket.emit("show_chitthi", chitthi2send);
+      waiting = UsersInWaiting(room);
+      io.to(room).emit("send_users", waiting);
+      const first = UserToStart(room);
+      if (socket.id === first.id) {
+        io.to(socket.id).emit("IsItMe");
+      }
+    } else {
+      socket.emit("redirect", destination);
+    }
+  });
+  socket.on("ChittiToPass", ({ chitthival, name, room }) => {
+    console.log(chitthival, name, room);
+    ChitthiPass(chitthival, name, room);
+    io.to(room).emit("update-chitthi");
+    //waiting = UsersInWaiting(room);
+    //io.to(room).emit("send_users", waiting);
+  });
+  socket.on("SendMeNewChitthi", ({ name, room }) => {
+    const chitthi2send = sendchitti(name, room);
+    socket.emit("show_chitthi", chitthi2send);
+  });
+  socket.on("disconnect", () => {
+    Offliner(id);
+    userleft = GetUserById(id);
+    if (userleft) {
+      waiting = UsersInWaiting(userleft.room);
+      io.to(userleft.room).emit("send_users", waiting);
+    }
+  });
+});
+PORT = 3000 || process.env.PORT;
+HttpServer.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
---
+++ views/js/game.js
@@ -0,0 +1,63 @@
+var socket = io();
+var name = getParameterByName("name");
+var room = getParameterByName("room");
+var pass;
+console.log(name, room);
+socket.emit("Start-game", { name, room });
+socket.on("redirect", (destination) => {
+  window.location.href = destination;
+});
+socket.on("show_chitthi", (chitthi2send) => {
+  chitthi2send.forEach((chitthi) => {
+    ShowChitti(chitthi);
+  });
+});
+socket.on("send_users", (waiting) => {
+  ShowUsers(waiting);
+});
+socket.on("IsItMe", () => {
+  console.log("yes");
+  pass = true;
+});
+socket.on("update-chitthi", () => {
+  console.log("yea");
+  clear();
+  socket.emit("SendMeNewChitthi", { name, room });
+  alert("updated");
+});
+function clear() {
+  document.getElementById("chitti-container").innerHTML = "";
+}
+function ShowChitti(chitthi) {
+  console.log(chitthi);
+  const div = document.createElement("div");
+  div.classList.add("chitti");
+  div.innerHTML = `
+    <label for ="${chitthi}">${chitthi}<label>
+    <input type = "radio" id = "${chitthi}" name = "chitthi" value = "${chitthi}">`;
+  document.querySelector(".chitti-container").appendChild(div);
+}
+function ShowUsers(name) {
+  var userlist = document.getElementById("users");
+  userlist.innerHTML = "";
+  name.forEach((element) => {
+    userlist.innerHTML += `${element}<br>`;
+  });
+}
+function SendChittiToServer() {
+  var chitthival = document.querySelector(
+    'input[name="chitthi"]:checked'
+  ).value;
+  if (pass === true) {
+    socket.emit("ChittiToPass", { chitthival, name, room });
+    pass = false;
+  }
+}
+function getParameterByName(name, url = window.location.href) {
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
